507a1c6f647cd35b6d6e968f31ab027d361e7a88
DoggyStyleOnly <123248262+Carl-97@users.noreply.github.com>
2023-09-01 11:23:57 +0200

    fixes

---
+++ Functions.h
@@ -0,0 +1,14 @@
+//
+// Created by Caaaarl on 2023-09-01.
+//
+
+#ifndef TESTING_FUNCTIONS_H
+#define TESTING_FUNCTIONS_H
+
+//Write your functions here
+
+int add(int x, int y) {
+    return x+y;
+}
+
+#endif //TESTING_FUNCTIONS_H
