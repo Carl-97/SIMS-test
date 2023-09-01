53fa38b4712c835569db694807f7007865c4809b
DoggyStyleOnly <123248262+Carl-97@users.noreply.github.com>
2023-09-01 11:20:52 +0200

    fixes

--- main.cpp
+++ main.cpp
@@ -5,25 +5,13 @@
 #include "SortAlgortihms.h"
 #include "time.h"
 
-//Seed generator for random Integers
-double rng() {
-    static std::seed_seq seed{static_cast<long unsigned int>(time(nullptr))};
-    static std::mt19937 mersenne_engine{seed};
-    static std::uniform_real_distribution<double> dist(0.0, 10000.0);
-    return dist(mersenne_engine);
-}
+/**
+ * Welcome to Branch 2
+ *
+ */
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-
-    //Creating and filling a vector with 1000 random Integers and sorting them using insertion sort
-    insertion insertion;
-    std::vector<double> data;
-    data.resize(1000);
-    std::cout << *data.begin() << " " << *data.end() << "\n";
-    insertion(data.begin(), data.end());
-    assert(std::is_sorted(data.begin(),data.end()));
-    std::cout << *data.begin() << " " << *data.end();
+    std::cout << "Hello, Branch2!" << std::endl;
 
     return 0;
 }
